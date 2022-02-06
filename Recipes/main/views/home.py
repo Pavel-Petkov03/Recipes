c86b4072f40f2532ae9a3d37e23b062958c78d9a
Pavel Petkov <elias5247698@abv.bg>
2022-02-06 14:08:58 +0200

    final commit

--- Recipes/main/views/home.py
+++ Recipes/main/views/home.py
@@ -6,4 +6,5 @@ from Recipes.main.models import Recipe
 
 class HomeView(View):
     def get(self, request):
+        print(View)
         return render(request, 'index.html', {"recipes": Recipe.objects.all()})
